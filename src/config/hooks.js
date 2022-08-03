e6d6e83635c33ecde62f99f90048ea8a90cae15e
Kerem AKSOY <keremaksoy1127@gmail.com>
2022-08-03 14:22:30 +0300

    Add the Router

---
+++ src/config/hooks.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "./firebase"
+
+export const useIsLoggedIn =()=>{
+    const [isLoggedIn, setIsLoggedIn] = useState(null)
+    
+    useEffect(()=>{
+       onAuthStateChanged(auth, (user) =>{
+        console.log(user)
+        setIsLoggedIn(!!user)
+       })
+ }, [])
+    return isLoggedIn;
+}
--- src/config/router.js
+++ src/config/router.js
@@ -1,14 +1,22 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignUp from "../components/SignUp"
 import SignIn from "../components/SignIn"
+import Main from "../components/Main"
+import AuthLayout from "../layout/AuthLayout"
+import Layout from "../layout/Layout"
 
-export default function Router(){
-    return (
- <BrowserRouter>
- <Routes>
-    <Route path="/" element={<SignIn/>}/>
-    <Route path="/sign-up" element={<SignUp/>}/>
-    </Routes>
- </BrowserRouter>
-    )
+export default function Router() {
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route element={<Layout />} >
+               <Route path="/" element={<Main />} />
+            </Route>
+            <Route element={<AuthLayout />}>
+               <Route path="/sign-in" element={<SignIn />} />
+               <Route path="/sign-up" element={<SignUp />} />
+            </Route>
+         </Routes>
+      </BrowserRouter>
+   )
 }
