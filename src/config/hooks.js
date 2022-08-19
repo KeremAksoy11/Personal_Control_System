342ad476d0a3c698786d79774b1ff98ac93fbb74
Kerem AKSOY <keremaksoy1127@gmail.com>
2022-08-19 16:57:56 +0300

    I added profile information and logout button

--- src/config/hooks.js
+++ src/config/hooks.js
@@ -2,14 +2,25 @@ import { useState, useEffect } from "react"
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "./firebase"
 
-export const useIsLoggedIn =()=>{
+export const useCurrentUser = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        });
+    }, [])
+
+    return user;
+};
+
+export const useIsLoggedIn = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(null)
-    
-    useEffect(()=>{
-       onAuthStateChanged(auth, (user) =>{
-        console.log(user)
-        setIsLoggedIn(!!user)
-       })
- }, [])
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, (user) => {
+            setIsLoggedIn(!!user);
+        });
+    }, [])
     return isLoggedIn;
 }
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,32 @@
 import { Outlet, Navigate } from "react-router-dom"
-import { useIsLoggedIn } from "../config/hooks"
+import { useCurrentUser, useIsLoggedIn } from "../config/hooks"
 import Loading from "../components/Loading"
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import {useDispatch} from "react-redux"
+import { logOut } from "../redux/authSlice"
+import { useSelector } from "react-redux";
 
 export default function Layout() {
+
+    const currentUser = useCurrentUser()
     const isLoggedIn = useIsLoggedIn();
+
+    const [showUser, setShowUser] = useState(false);
+
+    const handleCloseUser = () => setShowUser(false);
+    const handleShowUser = () => setShowUser(true);
+
+    const [showClose, setShowClose] = useState(false);
+
+    const handleCloseClose = () => setShowClose(false);
+    const handleShowClose = () => setShowClose(true);
+
+    const dispatch = useDispatch();
+
+    const name = useSelector((state) => state.auth.name)
+    
 
     if (isLoggedIn === null) return <Loading />;
     else if (isLoggedIn === false) return <Navigate replace to="/sign-in" />
@@ -22,25 +45,57 @@ export default function Layout() {
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">Personeller</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
+                                <a class="nav-link" href="#">Toplantılar</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
+                                <a class="nav-link" href="#">Duyurular</a>
                             </li>
                         </ul>
                     </div>
                     <div class="dropdown">
-                        <a class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" id="dropdownMenuLink"  aria-haspopup="true" aria-expanded="false" ><img src="https://freesvg.org/img/abstract-user-flat-1.png" alt="" width="35" height="35" class="d-inline-block align-text-top" /></a>
+                        <a class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" id="dropdownMenuLink" aria-haspopup="true" aria-expanded="false" ><img src="https://freesvg.org/img/abstract-user-flat-1.png" alt="" width="35" height="35" class="d-inline-block align-text-top" /></a>
 
-                        <ul class="dropdown-menu dropdown-menu-end"  aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Link 1</a></li>
-                            <li><a class="dropdown-item" href="#">Link 2</a></li>
-                            <li><a class="dropdown-item" href="#">Link 3</a></li>
+
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            
+                            <Button variant="black" onClick={handleShowUser}>
+                            Kişisel Bilgilerim
+                            </Button>
+                            <Modal show={showUser} onHide={handleCloseUser}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Kişisel Bilgilerim</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                               İsim: {name}
+                               <hr />
+                               Email:  {currentUser.email}
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="danger" onClick={handleCloseUser}>
+                                        Kapat
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+
                             <li><hr class="dropdown-divider" /></li>
-                            <li><a class="dropdown-item" href="#">Another link</a></li>
+                            <Button variant="black" onClick={handleShowClose}>
+                            Çıkış Yap
+                            </Button>
+                            <Modal show={showClose} onHide={handleCloseClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Çıkış Yapmak İstediğinizden Emin Misiniz?</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Footer>
+                                    <Button variant="danger" onClick={()=>
+                                        
+                                        dispatch(logOut())}>
+                                        Evet
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
                         </ul>
                     </div>
                     <div />
