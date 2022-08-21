c917cc99c60d7596f848cebe09f0be01a7466a34
Kerem AKSOY <keremaksoy1127@gmail.com>
2022-08-21 17:38:15 +0300

    I made new additions

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,19 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { addPersonal, deletePersonal } from '../redux/personalSlice'
 import { usePersonalLister } from "../config/firebase"
 import '../components/table.css'
-import {changeDraftPersonalName,
+import {
+    changeDraftPersonalName,
     changeDraftPersonalSurname,
     changeDraftPersonalBirthday,
     changeDraftPersonalStartDate,
     changeDraftPersonalDepartment,
     changeDraftPersonalPhone,
     changeDraftPersonalMail
-  } from "../redux/personalSlice"
+} from "../redux/personalSlice"
 
-  import Button from 'react-bootstrap/Button';
-  import Modal from 'react-bootstrap/Modal';
-  import Form from 'react-bootstrap/Form';
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/esm/Col";
 
 function Main() {
     usePersonalLister()
@@ -37,134 +39,143 @@ function Main() {
     const phone = useSelector((state) => state.personal.draftPersonal.phone);
     const mail = useSelector((state) => state.personal.draftPersonal.mail);
 
-    
-    const handleNameChange = (e) =>{
+
+    const handleNameChange = (e) => {
         dispatch(changeDraftPersonalName(e.currentTarget.value))
     }
 
-    const handleSurnameChange = (e) =>{
+    const handleSurnameChange = (e) => {
         dispatch(changeDraftPersonalSurname(e.currentTarget.value))
     }
 
-    const handleBirthdayChange = (e) =>{
+    const handleBirthdayChange = (e) => {
         dispatch(changeDraftPersonalBirthday(e.currentTarget.value))
     }
-    const handleStartDateChange = (e) =>{
+    const handleStartDateChange = (e) => {
         dispatch(changeDraftPersonalStartDate(e.currentTarget.value))
     }
-    const handleDepartmentChange = (e) =>{
+    const handleDepartmentChange = (e) => {
         dispatch(changeDraftPersonalDepartment(e.currentTarget.value))
     }
-    const handlePhoneChange = (e) =>{
+    const handlePhoneChange = (e) => {
         dispatch(changeDraftPersonalPhone(e.currentTarget.value))
     }
-    const handleMailChange = (e) =>{
+    const handleMailChange = (e) => {
         dispatch(changeDraftPersonalMail(e.currentTarget.value))
     }
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        dispatch(addPersonal({name, surname, birthday, startDate, department, phone, mail}))
-      }
-    
+    const handleSubmit = (e) => {
+     
+        dispatch(addPersonal({ name, surname, birthday, startDate, department, phone, mail }))
+    }
+
     return (
-        
 
-        
+
+
         <div className="container">
-            <Button className="btn btn-success" onClick={handleShowPersonalModal}>
-                            Personel Ekle
-            </Button>
+           
 
-                            { <Modal show={showPersonal} onHide={handleClosePersonalModal}>
-                                <Modal.Header closeButton>
-                                    <Modal.Title>Personel Ekleme</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Label>İsim</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handleNameChange}
-                                    value = {name}
-                                    required 
-                               
-                                    />
+            {<Modal show={showPersonal} onHide={handleClosePersonalModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Personel Ekleme</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col sm={12}>
+                    <Form onSubmit={handleSubmit}>
+                        <p className="text-center" style={{ color: "#39ace7" }}>İsim</p>
+                        <Form.Control 
+                        size = "sm"
+                            type="text"
+                            name="addPersonal"
+                            onChange={handleNameChange}
+                            value={name}
+                            required
+                            autoFocus
 
-                                    <Form.Label>Soy Ad</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handleSurnameChange}
-                                    value = {surname}
-                                    required 
-                                    />
+                        />
 
-                                    <Form.Label>Doğum Tarihi</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handleBirthdayChange}
-                                    value = {birthday}
-                                    required 
-                                
-                                    />
+                        <p className="text-center" style={{ color: "#39ace7" }}>Soy İsim</p>
+                        <Form.Control
+                            type="text"
+                            name="addPersonal"
+                            onChange={handleSurnameChange}
+                            value={surname}
+                            required
+                        />
 
-                                    <Form.Label>İşe Başlangıç Tarihi</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handleStartDateChange}
-                                    value = {startDate}
-                                    required 
-                               
-                                    />
+                        <p className="text-center" style={{ color: "#39ace7" }}>Doğum Tarihi</p>
+                        <Form.Control
+                            type="text"
+                            name="addPersonal"
+                            onChange={handleBirthdayChange}
+                            value={birthday}
+                            required
 
-                                    <Form.Label>Bölüm</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handleDepartmentChange}
-                                    value = {department}
-                                    required 
-                                    
-                                    />
+                        />
 
-                                    <Form.Label>Telefon Numarası</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handlePhoneChange}
-                                    value = {phone}
-                                    required 
-                                   
-                                    />
+                        <p className="text-center" style={{ color: "#39ace7" }}>İşe Başlangıç Tarihi</p>
+                        <Form.Control
+                            type="text"
+                            name="addPersonal"
+                            onChange={handleStartDateChange}
+                            value={startDate}
+                            required
+
+                        />
+                        <p className="text-center" style={{ color: "#39ace7" }}>Bölüm</p>
+                        <Form.Control
+                            type="text"
+                            name="addPersonal"
+                            onChange={handleDepartmentChange}
+                            value={department}
+                            required
+
+                        />
+
+                        <p className="text-center" style={{ color: "#39ace7" }}>Telefon Numarası</p>
+                        <Form.Control
+                            type="text"
+                            name="addPersonal"
+                            onChange={handlePhoneChange}
+                            value={phone}
+                            required
+
+                        />
 
 
-                                    <Form.Label>Mail</Form.Label>
-                                    <Form.Control 
-                                    type ="text"
-                                    name ="addPersonal"
-                                    onChange={handleMailChange}
-                                    value = {mail}
-                                    required 
-                              
-                                    />
-                                    <Form.Group>
-                                        <Button variant="success" onClick={handleClosePersonalModal} type="submit">Personeli Ekle</Button>
-                                    </Form.Group>
-                                </Form>
-                                </Modal.Body>
-                                <Modal.Footer>
-                                    <Button variant="danger" onClick={handleClosePersonalModal}>
-                                        Kapat
-                                    </Button>
-                                </Modal.Footer>
-                            </Modal> }
+                        <p className="text-center" style={{ color: "#39ace7" }}>Mail</p>
+                        <Form.Control
+                            type="text"
+                            name="addPersonal"
+                            onChange={handleMailChange}
+                            value={mail}
+                            required
 
-            <button onClick={() => dispatch(addPersonal())}>Ekle</button>
+                        />
+                        <Form.Group>
+                            <Button variant="success" onClick={handleClosePersonalModal} type="submit">Personeli Ekle</Button>
+                        </Form.Group>
+                    </Form>
+                    </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClosePersonalModal}>
+                        Kapat
+                    </Button>
+                </Modal.Footer>
+            </Modal>}
+           
             <table>
+                
                 <thead>
+                <div>
+            <a href={() => false} onClick={handleShowPersonalModal}>
+            <img src="https://www.svgrepo.com/show/121235/add-user.svg" alt="" width="55" height="50" className="d-inline-block align-text-top" />
+            </a>
+            </div>
+            
                     <tr>
                         <th>İsim</th>
                         <th>Soy İsim</th>
@@ -174,10 +185,12 @@ function Main() {
                         <th>Telefon Numarası</th>
                         <th>E Posta</th>
                         <th>Sil</th>
+                        
                     </tr>
                 </thead>
-            {personal.map((personal) =>
                
+                {personal.map((personal) =>
+
                     <tbody>
                         <tr>
                             <td>{personal.name}</td>
@@ -187,15 +200,15 @@ function Main() {
                             <td>{personal.department}</td>
                             <td>{personal.phone}</td>
                             <td>{personal.mail}</td>
-                            <a  href={() => false} onClick={() => dispatch(deletePersonal(personal.id))}>
+                            <a href={() => false} onClick={() => dispatch(deletePersonal(personal.id))}>
 
                                 <img src="https://img.icons8.com/fluency/344/delete-forever.png" alt="" width="55" height="50" className="d-inline-block align-text-top" />
-                             
+
                             </a>
                         </tr>
                     </tbody>
-            
-            )}
+
+                )}
             </table>
         </div>
     );
